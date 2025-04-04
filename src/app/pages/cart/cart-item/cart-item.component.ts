f0045dc0013c90bcea18b0529a664896b4f97e4c
matheus98rocha <matheus98rocha@gmail.com>
2025-04-03 23:51:57 -0300

    feat: responsive

--- src/app/pages/cart/cart-item/cart-item.component.ts
+++ src/app/pages/cart/cart-item/cart-item.component.ts
@@ -8,22 +8,27 @@ import { QuantityInputComponent } from '../../../components/quantity-input/quant
   selector: 'app-cart-item',
   imports: [PrimaryButtonComponent, QuantityInputComponent],
   template: `
-    <div class="bg-white shadow-md rounded-xl p-6 flex gap-4 items-center">
-      <img [src]="item().image" class="w-[50px] h-[50px] object-contain" />
-      <div class="flex flex-col">
-        <span class="text-md font-bold">{{ item().title }}</span>
-        <span class="text-sm"> {{ '$' + item().price }}</span>
+    <div
+      class="bg-white shadow-md rounded-xl p-6 flex justify-between flex-col lg:flex-row gap-4 items-center"
+    >
+      <div class="flex gap-4 items-center">
+        <img [src]="item().image" class="w-[50px] h-[50px] object-contain" />
+        <div class="flex flex-col">
+          <span class="text-md font-bold">{{ item().title }}</span>
+          <span class="text-sm"> {{ '$' + item().price }}</span>
+        </div>
       </div>
-      <div class="flex-1"></div>
-      <app-quantity-input
-        (handleIncrement)="cartService.incrementItem(item())"
-        (handleDecrement)="cartService.decrementItem(item())"
-        [qtd]="item().qtd"
-      />
-      <app-primary-button
-        label="Remove"
-        (buttonClick)="cartService.removeFromCart(item())"
-      />
+      <div class="flex gap-4 items-center">
+        <app-quantity-input
+          (handleIncrement)="cartService.incrementItem(item())"
+          (handleDecrement)="cartService.decrementItem(item())"
+          [qtd]="item().qtd"
+        />
+        <app-primary-button
+          label="Remove"
+          (buttonClick)="cartService.removeFromCart(item())"
+        />
+      </div>
     </div>
   `,
   styles: ``,
